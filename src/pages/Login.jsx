a52c6c4211ec4e99e3912e24f4125608498ffcba
Jordan Aston <gcab022124@coderacademy.edu.au>
2023-07-25 09:10:27 +1000

    login page implemented with api

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import LoginForm from "../components/LoginForm";
 
 function Login() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <p className="text-center mt-20 text-xl flex-grow">Login</p>
+        <div className="flex flex-col items-center justify-center flex-grow">
+          <p className="text-center text-xl mb-6 font-roboto-mono text-voyage-black">Login</p>
+          <LoginForm />
+        </div>
       <Footer />
     </div>
   );
