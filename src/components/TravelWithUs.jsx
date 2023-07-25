09a7109bb2894de1bf68edf5e10da6184b9a3340
Jordan Aston <gcab022124@coderacademy.edu.au>
2023-07-25 10:29:51 +1000

    travel with us component built and added to home page

---
+++ src/components/TravelWithUs.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function TravelWithUs() {
+  return (
+    <div className="mx-auto px-6 sm:px-16 md:px-16 lg:px-44 max-w-screen-xl text-center space-y-8 mb-8 lg:mb-12 text-voyage-black text-sm sm:text-sm md:text-md lg:text-lg">
+      <h1 className="font-roboto font-normal pt-8 lg:pt-12">TRAVEL WITH US</h1>
+      <p className="font-roboto-mono text-xs sm:text-sm md:text-md lg:text-lg">
+        Vintage camper-vans, meticulously refurbished and uniquely designed,
+        available for hire. These are self-sustaining mobile cottages -
+        conveniently situated in Location, Location, Australia.
+      </p>
+
+      <div className="flex justify-center">
+        <li className="list-none border border-voyage-black font-normal px-2 py-1">
+          <NavLink
+            to="/vans"
+            className="transition-all duration-500 hover:font-normal"
+          >
+            BOOK NOW
+          </NavLink>
+        </li>
+      </div>
+    </div>
+  );
+}
+
+export default TravelWithUs;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,21 +2,35 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import HomePage1 from "../assets/images/home-page-1.png";
+import HomePage2 from "../assets/images/home-page-2.png";
+import HomePage3 from "../assets/images/home-page-3.png";
+import HomePage4 from "../assets/images/home-page-4.png";
+import TravelWithUs from "../components/TravelWithUs";
 
 function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      
+
       <div>
         <img src={HomePage1} alt="Home Page" className="w-full h-auto" />
       </div>
-      
+
+      <TravelWithUs />
+
+      <div>
+        <img src={HomePage2} alt="Home Page" className="w-full h-auto" />
+      </div>
+      <div>
+        <img src={HomePage3} alt="Home Page" className="w-full h-auto" />
+      </div>
+      <div>
+        <img src={HomePage4} alt="Home Page" className="w-full h-auto" />
+      </div>
+
       <Footer />
     </div>
   );
 }
 
 export default Home;
-
-
