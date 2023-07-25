6d8a1332a4794fdc11c6dbf8111f4b2c1925727a
James Boland <102449868+JRBoland@users.noreply.github.com>
2023-07-25 10:27:21 +1000

    Updated Vans page

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -11,10 +11,17 @@ function Vans() {
     <div className="flex flex-col min-h-screen">
       <Header />
       <div className="text-center mt-20 text-xl flex-grow">
-      <VansSelect className="text-center mt-20 text-xl font-roboto font-normal w-fit"onVanSelect={setSelectedVan} />
-      {selectedVan && <p className="text-center mt-20 text-xl font-roboto font-black">{selectedVan.vanName}</p>}
+        <VansSelect
+          className="text-center mt-20 text-xl font-roboto font-normal w-fit"
+          onVanSelect={setSelectedVan}
+        />
+        {selectedVan && (
+          <p className="text-center mt-20 text-xl font-roboto font-black">
+            {selectedVan.vanName}
+          </p>
+        )}
       </div>
-      {selectedVan && <BookingCalendar vanID={selectedVan._id}/>}
+      {selectedVan && <BookingCalendar vanID={selectedVan._id} />}
       <Footer />
     </div>
   )
