3d67f423a7c257b3d09bef4112ca3db5b103fa27
Jordan Aston <gcab022124@coderacademy.edu.au>
2023-07-28 10:00:33 +1000

    change password form implemented on account page, delete account button functionaltiy working

--- src/components/UpdateDetailsForm.jsx
+++ src/components/UpdateDetailsForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { UserContext } from '../components/UserContext';
+import { UserContext } from "../components/UserContext";
 
 function UpdateDetailsForm() {
   const [firstName, setFirstName] = useState("");
@@ -34,8 +34,8 @@ function UpdateDetailsForm() {
         updatedFields,
         {
           headers: {
-            Authorization: `Bearer ${token}`
-          }
+            Authorization: `Bearer ${token}`,
+          },
         }
       );
 
@@ -52,88 +52,99 @@ function UpdateDetailsForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col items-center px-5 space-y-4 text-voyage-black">
-      <div className="w-full shadow-lg">
-        <input
-          type="text"
-          id="firstName"
-          name="firstName"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-          placeholder="First Name..."
-          className="w-full h-10 pl-2 border-voyage-black border-opacity-50 focus:border-voyage-black focus:border-opacity-50 focus:outline-none focus:ring-2 focus:ring-voyage-black focus:ring-opacity-50 rounded"
-        />
+    <div className="flex justify-center">
+      <div className="max-w-screen-sm w-full lg:max-w-screen-md">
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col px-5 space-y-4 text-voyage-black"
+        >
+          <div className="w-full space-y-4">
+            <div className="flex space-x-4">
+              <input
+                type="text"
+                id="firstName"
+                name="firstName"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                placeholder="First Name..."
+                className="w-full h-10 pl-2 shadow-lg border-voyage-black border-opacity-50 focus:border-voyage-black focus:border-opacity-50 focus:outline-none focus:ring-2 focus:ring-voyage-black focus:ring-opacity-50 rounded"
+              />
 
-        <input
-          type="text"
-          id="lastName"
-          name="lastName"
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-          placeholder="Last Name..."
-          className="w-full h-10 pl-2 border-voyage-black border-opacity-50 focus:border-voyage-black focus:border-opacity-50 focus:outline-none focus:ring-2 focus:ring-voyage-black focus:ring-opacity-50 rounded"
-        />
+              <input
+                type="text"
+                id="lastName"
+                name="lastName"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                placeholder="Last Name..."
+                className="w-full h-10 pl-2 shadow-lg border-voyage-black border-opacity-50 focus:border-voyage-black focus:border-opacity-50 focus:outline-none focus:ring-2 focus:ring-voyage-black focus:ring-opacity-50 rounded"
+              />
+            </div>
 
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email..."
-          className="w-full h-10 pl-2 border-voyage-black border-opacity-50 focus:border-voyage-black focus:border-opacity-50 focus:outline-none focus:ring-2 focus:ring-voyage-black focus:ring-opacity-50 rounded"
-        />
+            <div className="flex space-x-4">
+              <input
+                type="date"
+                id="dob"
+                name="dob"
+                value={dob}
+                onChange={(e) => setDob(e.target.value)}
+                placeholder="D.O.B..."
+                className="w-full h-10 pl-2 shadow-lg border-voyage-black border-opacity-50 focus:border-voyage-black focus:border-opacity-50 focus:outline-none focus:ring-2 focus:ring-voyage-black focus:ring-opacity-50 rounded"
+              />
 
-        <input
-          type="tel"
-          id="phoneNumber"
-          name="phoneNumber"
-          value={phoneNumber}
-          onChange={(e) => setPhoneNumber(e.target.value)}
-          placeholder="Phone Number..."
-          className="w-full h-10 pl-2 border-voyage-black border-opacity-50 focus:border-voyage-black focus:border-opacity-50 focus:outline-none focus:ring-2 focus:ring-voyage-black focus:ring-opacity-50 rounded"
-        />
+              <input
+                type="tel"
+                id="phoneNumber"
+                name="phoneNumber"
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+                placeholder="Phone Number..."
+                className="w-full h-10 pl-2 shadow-lg border-voyage-black border-opacity-50 focus:border-voyage-black focus:border-opacity-50 focus:outline-none focus:ring-2 focus:ring-voyage-black focus:ring-opacity-50 rounded"
+              />
+            </div>
 
-        <input
-          type="text"
-          id="address"
-          name="address"
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
-          placeholder="Address..."
-          className="w-full h-10 pl-2 border-voyage-black border-opacity-50 focus:border-voyage-black focus:border-opacity-50 focus:outline-none focus:ring-2 focus:ring-voyage-black focus:ring-opacity-50 rounded"
-        />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email..."
+              className="w-full h-10 pl-2 shadow-lg border-voyage-black border-opacity-50 focus:border-voyage-black focus:border-opacity-50 focus:outline-none focus:ring-2 focus:ring-voyage-black focus:ring-opacity-50 rounded"
+            />
 
-        <input
-          type="date"
-          id="dob"
-          name="dob"
-          value={dob}
-          onChange={(e) => setDob(e.target.value)}
-          placeholder="D.O.B..."
-          className="w-full h-10 pl-2 border-voyage-black border-opacity-50 focus:border-voyage-black focus:border-opacity-50 focus:outline-none focus:ring-2 focus:ring-voyage-black focus:ring-opacity-50 rounded"
-        />
+            <input
+              type="text"
+              id="address"
+              name="address"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              placeholder="Address..."
+              className="w-full h-10 pl-2 shadow-lg border-voyage-black border-opacity-50 focus:border-voyage-black focus:border-opacity-50 focus:outline-none focus:ring-2 focus:ring-voyage-black focus:ring-opacity-50 rounded"
+            />
 
-        <input
-          type="text"
-          id="driversLicense"
-          name="driversLicense"
-          value={driversLicense}
-          onChange={(e) => setDriversLicense(e.target.value)}
-          placeholder="Driver's License..."
-          className="w-full h-10 pl-2 border-voyage-black border-opacity-50 focus:border-voyage-black focus:border-opacity-50 focus:outline-none focus:ring-2 focus:ring-voyage-black focus:ring-opacity-50 rounded"
-        />
+            <input
+              type="text"
+              id="driversLicense"
+              name="driversLicense"
+              value={driversLicense}
+              onChange={(e) => setDriversLicense(e.target.value)}
+              placeholder="Driver's License..."
+              className="w-1/2 h-10 pl-2 shadow-lg border-voyage-black border-opacity-50 focus:border-voyage-black focus:border-opacity-50 focus:outline-none focus:ring-2 focus:ring-voyage-black focus:ring-opacity-50 rounded"
+            />
+          </div>
+          <div className="self-start">
+            <button
+              type="submit"
+              className="bg-voyage-green shadow-lg text-white font-roboto mb-10 font-light rounded px-4 py-2"
+            >
+              Update
+            </button>
+          </div>
+        </form>
       </div>
-      <div className="self-start">
-        <button type="submit" className="bg-voyage-green text-white font-roboto font-light rounded px-4 py-2">Update</button>
-      </div>
-    </form>
+    </div>
   );
 }
 
 export default UpdateDetailsForm;
-
-
-
-
-
--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import PersonalDetails from "../components/PersonalDetails";  // Import the PersonalDetails component
-import UpdateDetailsForm from "../components/UpdateDetailsForm";  // Import the UpdateDetailsForm component
+import PersonalDetails from "../components/PersonalDetails";  
+import UpdateDetailsForm from "../components/UpdateDetailsForm";  
+import ChangePasswordForm from "../components/ChangePasswordForm";
 
 function Account() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <p className="text-center font-roboto-mono text-voyage-black mt-20 mb-16 text-xl">Account Settings</p>
-      <PersonalDetails /> {/* Use the PersonalDetails component here */}
-      <p className="text-center font-roboto-mono text-voyage-black mt-20 mb-16 text-xl">Update Personal Details</p>
-      <UpdateDetailsForm /> {/* Use the UpdateDetailsForm component here */}
+      <p className="text-center font-roboto-mono text-voyage-black mt-20 text-xl">Account Settings</p>
+      <p className="text-center font-roboto-mono text-voyage-black mt-20 mb-10 text-md">Personal Details</p>
+      <PersonalDetails /> 
+      <p className="text-center font-roboto-mono text-voyage-black mt-20 mb-10 text-md">Update Personal Details</p>
+      <UpdateDetailsForm /> 
+      <p className="text-center font-roboto-mono text-voyage-black mb-10 text-md">Change Password</p>
+      <ChangePasswordForm />
       <Footer />
     </div>
   );
