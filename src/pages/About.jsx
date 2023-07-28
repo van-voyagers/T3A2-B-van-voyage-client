60e6b6bdab19ea326129d082baa40d0cf4cc4bba
Jordan Aston <gcab022124@coderacademy.edu.au>
2023-07-28 10:16:08 +1000

    about page built out

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import AboutPage1 from "../assets/images/about-page-1.png";
+import AboutPage2 from "../assets/images/about-page-2.png";
+import AboutPageContent from "../components/AboutPageContent";
 
 function About() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <p className="text-center mt-20 text-xl flex-grow">About</p>
+      <div>
+        <img src={AboutPage1} alt="Home Page" className="w-full h-auto" />
+      </div>
+      <AboutPageContent />
+      <div>
+        <img src={AboutPage2} alt="Home Page" className="w-full h-auto" />
+      </div>
       <Footer />
     </div>
   );
