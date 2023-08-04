5d856be77f33fcad012a06ec1c6a16ce6b8cc884
Jordan Aston <gcab022124@coderacademy.edu.au>
2023-08-04 12:04:10 +1000

    padding edit

--- src/components/TravelWithUs.jsx
+++ src/components/TravelWithUs.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 function TravelWithUs() {
   return (
     <div className="mx-auto px-6 sm:px-16 md:px-16 lg:px-44 max-w-screen-xl text-center space-y-8 mb-8 lg:mb-12 text-voyage-black text-sm sm:text-sm md:text-md lg:text-lg">
-      <h1 className="font-mono lg:text-xl font-normal pt-8 lg:pt-12 lg:text-xl">
+      <h1 className="font-mono font-normal pt-8 lg:pt-12 lg:text-xl">
         TRAVEL WITH US
       </h1>
       <p className="font-mono text-xs sm:text-sm md:text-md lg:text-lg">
